5b42d4beda61f5baa29c4741cf893e13c991edce
Sonozi1027 <153796135+Sonozi1027@users.noreply.github.com>
2024-06-19 18:24:24 +0900

    added these 2 files and edited database

---
+++ SampleBBS/src/main/java/servlet/EditFileServlet.java
@@ -0,0 +1,19 @@
+package servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+
+@WebServlet("/EditFileServlet")
+public class EditFileServlet extends HttpServlet{
+	//決まり文句
+		private static final long serialVersionUID = 1L;
+	
+		public EditFileServlet() {
+			super();
+		}
+	
+	public static void ImportFiles() {
+		
+	}
+}
